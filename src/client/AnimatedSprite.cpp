1ad685787aaf49a8383f83d8c2f5e3fb61e557a3
Unknown <Harry2.sollis@live.uwe.ac.uk>
2019-04-03 13:19:01 +0100

    Fixed the AnimatedSprite to use ScaledSprite.h

--- src/client/AnimatedSprite.cpp
+++ src/client/AnimatedSprite.cpp
@@ -4,34 +4,31 @@
 
 #include "AnimatedSprite.h"
 
-AnimatedSprite::AnimatedSprite()
-{
-
-}
+AnimatedSprite::AnimatedSprite() {}
 
 AnimatedSprite::~AnimatedSprite()
 {
-    delete sprite;
-    sprite = nullptr;
+  delete sprite;
+  sprite = nullptr;
 }
 
 void AnimatedSprite::setNewWidth(float new_width)
 {
-    sprite->scale(1);
-    sprite->width(new_width * GameResolution::scale);
-    bounding_box.width = sprite->width();
+  // sprite->scale(1);
+  sprite->width(new_width * GameResolution::scale);
+  bounding_box.width = sprite->width();
 }
 
 void AnimatedSprite::setNewHeight(float new_height)
 {
-    sprite->scale(1);
-    sprite->height(new_height * GameResolution::scale);
-    bounding_box.height = sprite->height();
+  // sprite->scale(1);
+  sprite->height(new_height * GameResolution::scale);
+  bounding_box.height = sprite->height();
 }
 
 void AnimatedSprite::setNewLoc(Vector2 new_loc)
 {
-    sprite->xPos(new_loc.x * GameResolution::scale);
-    sprite->yPos(new_loc.y * GameResolution::scale);
-    bounding_box.pos = Vector2(sprite->xPos(), sprite->yPos());
+  sprite->xPos(new_loc.x * GameResolution::scale);
+  sprite->yPos(new_loc.y * GameResolution::scale);
+  bounding_box.pos = Vector2(sprite->xPos(), sprite->yPos());
 }
--- src/client/AnimatedSprite.h
+++ src/client/AnimatedSprite.h
@@ -5,6 +5,7 @@
 #ifndef PROJECT_ANIMATEDSPRITE_H
 #define PROJECT_ANIMATEDSPRITE_H
 
+#include "UI/ScaledSprite.h"
 #include "client/Core/ServiceLocator.h"
 #include "gamelib/Constants.h"
 #include "gamelib/Math/BoundingBox.h"
@@ -12,26 +13,26 @@
 #include <Engine/Renderer.h>
 #include <Engine/Sprite.h>
 #include <string>
+
 class AnimatedSprite
 {
-public:
-    AnimatedSprite();
-    ~AnimatedSprite();
+ public:
+  AnimatedSprite();
+  ~AnimatedSprite();
 
-    void setNewWidth(float new_width);
-    float getWidth() { return sprite->width(); };
+  void setNewWidth(float new_width);
+  float getWidth() { return sprite->width(); };
 
-    void setNewHeight(float new_height);
-    float getHeight() { return sprite->height(); };
+  void setNewHeight(float new_height);
+  float getHeight() { return sprite->height(); };
 
-    void setNewLoc(Vector2 new_loc);
-    Vector2 getLoc() { return Vector2(sprite->xPos(), sprite->yPos()); };
+  void setNewLoc(Vector2 new_loc);
+  Vector2 getLoc() { return Vector2(sprite->xPos(), sprite->yPos()); };
 
-private:
-    ASGE::Renderer* renderer = nullptr;
-    ASGE::Sprite* sprite = nullptr;
-    BoundingBox bounding_box;
+ private:
+  ASGE::Renderer* renderer = nullptr;
+  ScaledSprite* sprite = nullptr;
+  BoundingBox bounding_box;
 };
 
-
-#endif //PROJECT_ANIMATEDSPRITE_H
+#endif // PROJECT_ANIMATEDSPRITE_H
