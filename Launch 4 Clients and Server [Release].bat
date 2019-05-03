59aaa65d52f4c13a003c61b48ec5f2ff2de61c3a
Matt Filer <matthew.filer@hotmail.co.uk>
2019-05-03 01:16:10 +0100

    release mode tweaks

---
+++ Launch 4 Clients and Server [Release].bat
@@ -0,0 +1,5 @@
+start cmake-build-release/build/GameClient/bin/GameClient.exe
+start cmake-build-release/build/GameClient/bin/GameClient.exe
+start cmake-build-release/build/GameClient/bin/GameClient.exe
+start cmake-build-release/build/GameClient/bin/GameClient.exe
+start cmake-build-release/build/GameServer/bin/GameServer.exe
